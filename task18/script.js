16b26ca4ceef4d28fd2f8ebfca47ba9a8f1f35c9
Xyasq001 <xiongyao001@foxmail.com>
2016-03-23 10:26:13 +0800

    task18

---
+++ task18/script.js
@@ -0,0 +1,48 @@
+/**
+ * Created by minexiong on 16/3/22.
+ */
+
+var a = new Array();
+
+function insertLeft(){
+    check();
+    var temp = document.getElementById('inputNum').value;
+    a.unshift(temp);
+    print();
+}
+function insertRight(){
+    check();
+    var temp = document.getElementById('inputNum').value;
+    a.push(temp);
+    print();
+}
+function delLeft(){
+    var temp = a.shift();
+    alert('将要删除的值为' + temp);
+    print();
+}
+function delRight(){
+    var temp = a.pop();
+    alert('将要删除的值为'+ temp);
+    print();
+}
+function check() {
+    var i = document.getElementById('inputNum').value;
+    if (isNaN(i) || i == ''){
+        alert('请输入数字');
+        return false;
+    }
+}
+function print(){
+    var insertEle = document.getElementById('text');
+    document.getElementById('text').innerHTML = '';
+    for(var i in a)
+        insertEle.innerHTML += '<span class="queue" >' + a[i] + '</span>';
+}
+function delEle(event){
+    var eleTar=event.target;
+    var eleValue = eleTar.innerHTML;
+    a.splice(a.indexOf(eleValue),1);
+    eleTar.parentNode.removeChild(eleTar);
+}
+
